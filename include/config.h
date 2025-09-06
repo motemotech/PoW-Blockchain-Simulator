4aba5352bee47026aab982840a887260dd0cecc2
motemotech <i.am.nicoshark@gmail.com>
2025-09-07 01:14:05 +0900

    got data

--- include/config.h
+++ include/config.h
@@ -28,7 +28,7 @@ constexpr ll ETH_TARGET_TIMESPAN = ETH_DIFFICULTY_ADJUSTMENT_INTERVAL * ETH_TARG
 
 namespace Config {
     constexpr int DEFAULT_NODE_COUNT = 1000;
-    constexpr bool DEFAULT_DYNAMIC_DIFFICULTY = false;
+    constexpr bool DEFAULT_DYNAMIC_DIFFICULTY = true;
     constexpr ll DEFAULT_DELAY = BTC_TARGET_GENERATION_TIME / 10;
     constexpr int DEFAULT_TIE_RULE = 0;  // デフォルトはfirst-seen rule
 
